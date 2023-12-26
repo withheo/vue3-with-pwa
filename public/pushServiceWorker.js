ea6c9377d0a7fd9acee334edfac2d7e802771a74
withHeo <mynet81@gmail.com>
2023-12-26 17:34:54 +0900

    FIX: allow app notification

--- public/pushServiceWorker.js
+++ public/pushServiceWorker.js
@@ -10,17 +10,29 @@
 
 
 
-self.addEventListener('install', () => {
+self.addEventListener('install', (e) => {
   console.log("Service worker Installing!");
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('activate', (e) => {
+  console.log("Service worker activated!");
 });
 
 
 // push 이벤트 정의
-self.addEventListener('push', (event) => {
+self.addEventListener('push', (event) =>{
   // 알림 데이터 받기
   const notificationData = event.data;
+  console.log(event);
+  // navigator.serviceWorker.getRegistrations().then((registration) => {
+  //   const res = registration && registration.length > 0 ? registration[0] : null;
+  //   res?.showNotification(notificationData.title, {
+  //     body: notificationData.message,
+  //   });
+  // });
 
-  // 알림 생성
+  // 알림 생성 //이거 쓰면 안될지도 모른다..
   const notification = new Notification(notificationData.title, {
     body: notificationData.message,
     icon: notificationData.icon
--- src/compositions/useNotification.ts
+++ src/compositions/useNotification.ts
@@ -14,12 +14,57 @@ const useNotification = () => {
     state.notiPermission = Notification.permission;
   }
 
+  function urlB64ToUint8Array(base64String: string)
+  {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i)
+      outputArray[i] = rawData.charCodeAt(i);
+    return outputArray;
+  }
+
   const requestPermission = () : Promise<boolean> => {
+    const convertedVPkey = urlB64ToUint8Array('BINxsPHrwAAIzNxfZRFVlQQ6jFvib0UOk4wjFThs_B_uy4rLOBCeaEyE1Qa6YdZIW6LNxf9FYRGGCFZRQEKmjxM');
     return new Promise((resolve, reject) => {
       Notification.requestPermission().then((result) => {
         state.notiPermission = result;
         if (result === "granted") {
-          resolve(true)
+
+          navigator.serviceWorker.ready.then(async (registration: any) => {
+            console.log(registration);
+            registration.pushManager.getSubscription().then(async (subscription: any) =>{
+              console.log("subscription : ", subscription);
+              if (!subscription) {
+                const t =  registration.pushManager.subscribe({
+                  userVisibleOnly: true,
+                  applicationServerKey: convertedVPkey,
+                }).then((x: any) => {
+
+                  console.log(x.endpoint);
+                  console.log(x.getKey('p256dh'));
+                  console.log(x.getKey('auth'));
+                  console.log(JSON.stringify(x));
+                });
+
+              }
+            }, (error: any) => {
+              console.error(error);
+            })
+          });
+          // navigator.serviceWorker.getRegistrations().then((registration: any) => {
+          //   registration.pushManager
+          //   .getSubscription()
+          //   .then(async (subscription: any) => {
+          //     // registration part
+          //     console.log("subscription , ", subscription)
+
+          //   });
+          // });
+          resolve(false)
         } else {
           resolve(false)
         }

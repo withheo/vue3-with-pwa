7bf0354ffe7085784cabc6ae8dfee8aa9af192d3
withHeo <mynet81@gmail.com>
2024-01-03 14:58:01 +0900

    FIX: workerpath

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,9 @@ import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 const app = createApp(OneSinal);
 app.use(I18n);
 app.use(OneSignalVuePlugin,{
-   appId: "c9aeceb5-0db3-4f92-b9e6-3e6a449b3db3",
+  appId: "c9aeceb5-0db3-4f92-b9e6-3e6a449b3db3",
   // appId: "eb4e2742-0bbd-41d8-a662-b9e600deec8f",
-  allowLocalhostAsSecureOrigin: false,
-  serviceWorkerPath: "push/onesignal/OneSignalSDKWorker.js",
+  allowLocalhostAsSecureOrigin: true,
+  serviceWorkerPath: "OneSignalSDKWorker.js",
 });
 app.mount('#app')

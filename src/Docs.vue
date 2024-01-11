1383fa64e3ba30dacbbafde1690b87ab94cb1cbe
withHeo <mynet81@gmail.com>
2024-01-11 18:56:44 +0900

    add: install docs2

--- src/Docs.vue
+++ src/Docs.vue
@@ -1,14 +1,14 @@
 <template>
   <div class ="py-2">
     <div v-if = "!state.isAndroid">
-      1. 구글 크롬으로 접속한다. 중앙 버튼 클릭
+      1. 사파리로 접속한다. 초록색 버튼 클릭
       <img src="images/doc_1.png" style="wdith:100%"/>
       <br>
       2. 홈 화면에 추가 하면 바탕화면에 설치 
       <img src="images/doc_2.png" style="wdith:100%"/>
     </div>
     <div v-else>
-      1. 사파리로 접속한다. 초록색 버튼 클릭
+      1. 구글 크롬으로 접속한다. 중앙 버튼 클릭
       <img src="images/doc_3.jpg" style="wdith:100%"/>
       <br>
       2. 앱설치 버튼 클릭 앱이 설치
